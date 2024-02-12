0cb0b618767c558fab188c86725ed598cd72ec14
12eu8oed <150359214+12eu8oed@users.noreply.github.com>
2024-02-12 18:26:30 +0900

    파일 덮어쓰기 방지 및 thumnailator 추가

--- src/main/java/com/seungho/good/UploadController.java
+++ src/main/java/com/seungho/good/UploadController.java
@@ -3,6 +3,7 @@ package com.seungho.good;
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.UUID;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -71,10 +72,10 @@ public class UploadController {
 		String uploadFolderPath = getFolder();
 
 		// make folder --------
-		File uploadPath = new File(uploadFolder, uploadFolderPath); //upload 밑에 현재날짜를 기반으로 폴더를 만들어주세용
+		File uploadPath = new File(uploadFolder, uploadFolderPath); // upload 밑에 현재날짜를 기반으로 폴더를 만들어주세용
 
-		if (uploadPath.exists() == false) //파일이 존재하지 않다면 
-			uploadPath.mkdirs();  //날짜 기반으로 파일을 만들어 주십시오
+		if (uploadPath.exists() == false) // 파일이 존재하지 않다면
+			uploadPath.mkdirs(); // 날짜 기반으로 파일을 만들어 주십시오
 		// make yyyy/MM/dd folder
 
 		for (MultipartFile multipartFile : uploadFile) {
@@ -84,11 +85,16 @@ public class UploadController {
 
 			String uploadFileName = multipartFile.getOriginalFilename();
 
+			log.info("자르기전 파일 이름: " + uploadFileName);
 			// IE has file path
 			uploadFileName = uploadFileName.substring(uploadFileName.lastIndexOf("\\") + 1);
+			log.info("only file name: " + uploadFileName);
+
+			UUID uuid = UUID.randomUUID(); // 랜덤값을 형성함
+			uploadFileName = uuid.toString() + "_" + uploadFileName; // 랜덤값_filename 으로 이름을 지어 같은 파일이 들어오더라도 파일이 덮어씌워지지않게 한다.
 
 //			File saveFile = new File(uploadFolder, uploadFileName);
-			File saveFile = new File(uploadPath, uploadFileName);  //날짜를 기반으로 만들어진 폴더 안에 사용자의 파일이 저장되어진다.
+			File saveFile = new File(uploadPath, uploadFileName); // 날짜를 기반으로 만들어진 폴더 안에 사용자의 파일이 저장되어진다.
 
 			try {
 				multipartFile.transferTo(saveFile);
